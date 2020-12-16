cffe139b66b5f939ddf77c3bb008b438026cf6f9
IgorSkobelev <iskobelev.consultant@newscorp.com>
2020-12-16 12:03:03 +0200

    MySQL schema example

--- schema_example/mysql_ddl.sql
+++ schema_example/mysql_ddl.sql
@@ -3,7 +3,7 @@
 
 -- Users
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	account_id INTEGER,
 	reputation INTEGER NOT NULL,
 	views INTEGER DEFAULT 0,
@@ -14,13 +14,13 @@ CREATE TABLE users (
 	profile_image_url VARCHAR(255),
 	website_url VARCHAR(255),
 	about_me TEXT,
-	creation_date TIMESTAMP NOT NULL,
-	last_access_date TIMESTAMP NOT NULL
+	creation_date TIMESTAMP(3) NOT NULL,
+	last_access_date TIMESTAMP(3) NOT NULL
 );
 
 -- Posts
 CREATE TABLE posts (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	owner_user_id INTEGER,
 	last_editor_user_id INTEGER,
 	post_type_id SMALLINT NOT NULL,
@@ -37,71 +37,72 @@ CREATE TABLE posts (
 	content_license VARCHAR(64) NOT NULL,
 	body TEXT,
 	favorite_count INTEGER,
-	creation_date TIMESTAMP NOT NULL,
-	community_owned_date TIMESTAMP,
-	closed_date TIMESTAMP,
-	last_edit_date TIMESTAMP,
-	last_activity_date TIMESTAMP
+	creation_date TIMESTAMP(3) NOT NULL,
+	community_owned_date TIMESTAMP(3),
+	closed_date TIMESTAMP(3),
+	last_edit_date TIMESTAMP(3),
+	last_activity_date TIMESTAMP(3),
+    
 );
 
 -- PostLinks
 CREATE TABLE post_links (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	related_post_id INTEGER NOT NULL,
 	post_id INTEGER NOT NULL,
-	link_type_id SMALLINT NOT NULL,
-	creation_date TIMESTAMP NOT NULL
+	link_type_id TINYINT NOT NULL,
+	creation_date TIMESTAMP(3) NOT NULL
 );
 
 -- PostHistory
 CREATE TABLE post_history (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	post_id INTEGER NOT NULL,
 	user_id INTEGER,
-	post_history_type_id SMALLINT NOT NULL,
+	post_history_type_id TINYINT NOT NULL,
 	user_display_name VARCHAR(64),
 	content_license VARCHAR(64),
 	revision_guid uuid,
 	text TEXT,
 	comment TEXT,
-	creation_date TIMESTAMP NOT NULL
+	creation_date TIMESTAMP(3) NOT NULL
 );
 
 -- Comments
 CREATE TABLE comments (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	post_id INTEGER NOT NULL,
 	user_id INTEGER,
-	score SMALLINT NOT NULL,
+	score TINYINT NOT NULL,
 	content_license VARCHAR(64) NOT NULL,
 	user_display_name VARCHAR(64),
 	text TEXT,
-	creation_date TIMESTAMP NOT NULL
+	creation_date TIMESTAMP(3) NOT NULL
 );
 
 -- Votes
 CREATE TABLE votes (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_id INTEGER,
 	post_id INTEGER NOT NULL,
-	vote_type_id SMALLINT NOT NULL,
-	bounty_amount SMALLINT,
-	creation_date TIMESTAMP NOT NULL
+	vote_type_id TINYINT NOT NULL,
+	bounty_amount TINYINT,
+	creation_date TIMESTAMP(3) NOT NULL
 );
 
 -- Badges
 CREATE TABLE badges (
-	id SERIAL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_id INTEGER NOT NULL,
-	class SMALLINT NOT NULL,
+	class TINYINT NOT NULL,
 	name VARCHAR(64) NOT NULL,
-	tag_based BOOL NOT NULL,
-	date TIMESTAMP NOT NULL
+	tag_based TINYINT(1) NOT NULL,
+	date TIMESTAMP(3) NOT NULL
 );
 
 -- Tags
 CREATE TABLE tags (
-	id SERIAL PRIMARY KEY,
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	excerpt_post_id INTEGER,
 	wiki_post_id INTEGER,
 	tag_name VARCHAR(255) NOT NULL,
